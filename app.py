70696ad49b0b9f066074c859df7f4a86527e6466
mortizcoilla <mortizcoilla@gmail.com>
2024-11-30 17:01:07 -0300

    corrige App2

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from dash import Dash
 import dash_bootstrap_components as dbc
-from layouts.datos_layout import layout as datos_layout
+from layouts.datos_layout import layout  # Importar el layout desde datos_layout
 
 # Inicialización de la aplicación
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server  # Esto asegura que Gunicorn pueda encontrar la aplicación WSGI
 
-# Configuración del layout (sin invocar como función)
-app.layout = datos_layout
+# Configuración del layout
+app.layout = layout
 
-# Registrar los callbacks
-from callbacks.datos_callbacks import update_content
+# Importar los callbacks
+import callbacks.datos_callbacks  # Asegúrate de que este archivo no tenga errores
 
-# Ejecutar la aplicación
+# Ejecutar la aplicación localmente
 if __name__ == "__main__":
     app.run_server(debug=True)
